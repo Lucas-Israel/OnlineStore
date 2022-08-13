ea866cf4a0b49d862cc0b37203b2f9353fff9298
Lucas Israel <104790267+Lucas-Israel@users.noreply.github.com>
2022-08-13 00:23:15 -0300

    Update ShoppingCart.jsx

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -141,7 +141,7 @@ class ShoppingCart extends Component {
             <div
               className="fs-5 mb-3"
             >
-              {`Total: ${currency.format(totalPrice)}`}
+              {cart.length > 'Total: R$0.00 ? 0 : `Total: ${currency.format(totalPrice)}`}
             </div>
             <Link to="/checkout">
               <button
